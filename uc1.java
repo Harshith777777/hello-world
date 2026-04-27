7c1ce7c2cc15d9c5fc5e6a24c45526491342129c
Harshith777777 <sb7904@srmist.edu.in>
2026-04-27 22:27:30 +0530

    Add files via upload

---
+++ uc1.java
@@ -0,0 +1,12 @@
+// UC1: Display Hello World on Console
+
+public class uc1 {
+    public static void main(String[] args) {
+
+        // Store message in variable (best practice)
+        String message = "Hello World";
+
+        // Print message
+        System.out.println(message);
+    }
+}
